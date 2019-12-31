c5fe2186ecd3f12019966782a151e0ef76f048aa
sk8053 <sk8053@nyu.edu>
2019-12-31 02:37:11 -0500

    simulation files

--- build/build-status.py
+++ build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3-dev-mc-loss-counter-debug', 'build/scratch/ns3-dev-mc-manynode-debug', 'build/scratch/ns3-dev-mc-downlink-loss-counter-debug', 'build/scratch/ns3-dev-ictc_ex-debug', 'build/scratch/ns3-dev-mc-scalability-debug', 'build/scratch/ns3-dev-mc-single-debug', 'build/scratch/ns3-dev-mc-downlink-debug', 'build/scratch/ns3-dev-mc-temp1-debug', 'build/scratch/ns3-dev-mc-gsoul-debug', 'build/scratch/ns3-dev-dn2018_2-b-debug']
+ns3_runnable_programs = ['build/scratch/ns3-dev-ictc_ex-debug', 'build/scratch/ns3-dev-dn2018_2-b-debug', 'build/scratch/ns3-dev-mc-single-debug', 'build/scratch/ns3-dev-mc-manynode-debug', 'build/scratch/ns3-dev-mc-downlink-loss-counter-debug', 'build/scratch/ns3-dev-mc-scalability-debug', 'build/scratch/ns3-dev-mc-gsoul-debug', 'build/scratch/ns3-dev-mc-temp1-debug', 'build/scratch/ns3-dev-mc-downlink-debug', 'build/scratch/ns3-dev-mc-loss-counter-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
